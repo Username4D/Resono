d459add6155d3fd3ffeae21e393e1796fca29508
Username4D <studio@kremzow.org>
2026-05-10 19:44:50 +0200

    added credits scene, and with that introduced some new additions to the standard theme, regarding the look of scrollbars

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -11,14 +11,19 @@ func _on_levels_button_pressed() -> void:
 	self.queue_free()
 
 func _on_settings_button_pressed() -> void:
-	print("wow")
 	ui_transition_handler.transition_start.emit()
 	await ui_transition_handler.transition_mid_point
-	var new_scene = await load("res://scenes/ui_elements/settings.tscn").instantiate()
+	var new_scene = await load("res://scenes/menu_scenes/settings.tscn").instantiate()
 	self.get_parent().add_child(new_scene)
 	await get_tree().process_frame
 	ui_transition_handler.transition_continue.emit()
 	self.queue_free()
 
 func _on_colors_button_pressed() -> void:
-	pass # Replace with function body.
+	ui_transition_handler.transition_start.emit()
+	await ui_transition_handler.transition_mid_point
+	var new_scene = await load("res://scenes/menu_scenes/credits.tscn").instantiate()
+	self.get_parent().add_child(new_scene)
+	await get_tree().process_frame
+	ui_transition_handler.transition_continue.emit()
+	self.queue_free()
